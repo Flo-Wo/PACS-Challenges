01dc3cfff592fa888abc98bdc909e414b3508ec1
Flo-Wo <florianwolf1710@t-online.de>
2024-04-17 17:09:40 +0200

    [DOCS] comment utilities

--- 02_SparseMatrix/include/utilities.hpp
+++ 02_SparseMatrix/include/utilities.hpp
@@ -7,17 +7,11 @@ namespace algebra {
 enum StorageOrder { row, col };
 enum NormOrder { frob, one, max };
 
-template <class T>
-double pow_integer(T base, unsigned int exp) {
-  double res = 1.0;
-  while (exp > 0) {
-    if (exp & 1) res *= base;
-    base *= base;
-    exp >>= 1;
-  }
-  return res;
-}
-
+/**
+ * @brief Compare two arrays of size 2 based on their second argument.
+ * 
+ * @tparam T Type of the entries.
+ */
 template <typename T>
 struct ColOrderComparator {
   bool operator()(const std::array<std::size_t, 2>& a,
@@ -29,6 +23,12 @@ struct ColOrderComparator {
   }
 };
 
+/**
+ * @brief Default, reimplementing lexicographical sorting, just for the sake
+ * of completeness.
+ * 
+ * @tparam T Type of the entries.
+ */
 template <typename T>
 struct RowOrderComparator {
   bool operator()(const std::array<std::size_t, 2>& a,
@@ -40,6 +40,16 @@ struct RowOrderComparator {
   }
 };
 
+/**
+ * @brief Utility function to generate a random vector of a fixed size
+ * for the benchmark test.
+ * 
+ * @tparam T Type of the entries.
+ * @param size Size of the vector.
+ * @param lower_bound Lower bounds to draw the values from
+ * @param upper_bound Upper bounds to draw the values from
+ * @return std::vector<T> Random vector.
+ */
 template <class T>
 std::vector<T> _generate_random_vector(std::size_t size,
                                        double lower_bound = -10,
